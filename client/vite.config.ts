10298ee6d0d669739bc2f60dda9db259223f51c6
Valentin Dosimont <valentin@pupucecorp.com>
2025-06-06 09:59:19 +0200

    fix: remove vite.config optimizeDeps

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,10 +19,6 @@ export default defineConfig({
 			"@": "/src",
 		},
 	},
-	optimizeDeps: {
-		include: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
-		exclude: ["@dojoengine/sdk"],
-	},
 	plugins: [
 		tailwindcss(),
 		TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
